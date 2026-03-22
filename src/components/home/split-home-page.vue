f22352f2b727c994fcf6bce6759ebb23ef06d127
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-03-22 14:59:23 +0100

    chore(spike): placeholder components

---
+++ src/components/home/split-home-page.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="metasafe-split-home-page">
+		<split-section @click="onClick(ProductEnum.Product1)" />
+		<split-section @click="onClick(ProductEnum.Product2)" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import SplitSection from './split-section.vue';
+
+/**
+ * Split section 50% / 50%
+ * On hover color change by default monochromatic and slightly blurred content
+ * On click navigation to product page
+ */
+
+enum ProductEnum {
+	Product1 = 'product1',
+	Product2 = 'product2',
+}
+
+function onClick(product: ProductEnum): void {
+	console.log('clicked');
+}
+</script>
+
+<style scoped>
+.metasafe-split-home-page {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+}
+</style>
---
+++ src/components/home/split-section.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="split-section">
+		<div class="split-section-content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.split-section {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	padding: 1rem 0;
+	align-items: center;
+	justify-content: center;
+
+	background: rgba(255, 255, 255, 0.8);
+	backdrop-filter: blur(5px);
+	-webkit-backdrop-filter: blur(5px);
+}
+
+.split-section:hover {
+	/* Remove default monochromatic color and slightly blurred overlay */
+	background: transparent;
+	backdrop-filter: none;
+	-webkit-backdrop-filter: none;
+}
+</style>
